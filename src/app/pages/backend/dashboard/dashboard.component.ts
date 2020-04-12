9786ec3f265933283d107c540d6cb7ef062b4920
niratchao <none66@gmail.com>
2020-04-12 17:15:25 +0700

    update final project

--- src/app/pages/backend/dashboard/dashboard.component.ts
+++ src/app/pages/backend/dashboard/dashboard.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ProductService } from '../../../services/product.service';
+ 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
-
+  //สร้างตัวแปรมารับค่าจาก API
+  dataProduct:any = []
+ 
+  constructor(public api: ProductService) { }
+ 
   ngOnInit(): void {
+    this.api.getProducts().subscribe((data: {}) => {
+      console.log(data);
+      this.dataProduct = data;
+    })
   }
-
+ 
+ 
 }
--- src/app/pages/login-register/login-register.component.ts
+++ src/app/pages/login-register/login-register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login-register',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginRegisterComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  //ตัวแปร
+  message: string = "Hello";
+  //ตัวแปร object
+  profile = {
+    "name": "Nirat",
+    "tel": "09393939393",
+    "gender": "Male"
   }
 
+  //2 way data binding
+  userData = {
+    "email":"",
+    "password":""
+  }
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+
+  }
+  submitLogin(){
+    // alert("hey");
+    if(this.userData.email == "admin@email.com" && this.userData.password == "1234") {
+      // alert("Login success");
+      this.router.navigate(["backend"])
+    } else {
+      alert("Login fail!!");
+    }
+  }
 }
